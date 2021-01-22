bac47129a678173eade38eda5b1b21a24716b247
canoe <ehgus430880@gmail.com>
2021-01-22 19:14:14 +0900

    editor master design

--- src/js/newPost.js
+++ src/js/newPost.js
@@ -1,5 +1,5 @@
-import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
-
+// import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import decouplededitor from "@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor";
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
 import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter";
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
@@ -21,11 +21,11 @@ import Font from "@ckeditor/ckeditor5-font/src/font";
 import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
 import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter";
 import CodeBlock from "@ckeditor/ckeditor5-code-block/src/codeblock";
-import SimpleBox from "./simpleBox/simplebox.js";
-import CKEditorInspector from "@ckeditor/ckeditor5-inspector";
+// import SimpleBox from "./simpleBox/simplebox.js";
+// import CKEditorInspector from "@ckeditor/ckeditor5-inspector";
 
 const $template = $(".template"),
-  $editorContainer = $(".editorContainer"),
+  $editorContainer = $(".document-editor"),
   $editorClose = $(".editorClose"),
   $selectorToggle = $(".selectorToggle"),
   $postSelectorList = $(".postSelectorList"),
@@ -36,86 +36,92 @@ let editorDoc; //에디터 인스턴스 저장 필드
 
 const domain = "localhost:3000";
 
-ClassicEditor.create(document.querySelector("#editor"), {
-  plugins: [
-    Essentials,
-    UploadAdapter,
-    Autoformat,
-    Bold,
-    Italic,
-    BlockQuote,
-    EasyImage,
-    Heading,
-    Image,
-    ImageCaption,
-    ImageStyle,
-    ImageToolbar,
-    ImageUpload,
-    Link,
-    List,
-    Paragraph,
-    Alignment,
-    Base64UploadAdapter,
-    // SimpleBox,
-    Font,
-    MediaEmbed,
-    CodeBlock,
-  ], // <--- MODIFIED
-  toolbar: {
-    items: [
-      "heading",
-      "|",
-      "bold",
-      "italic",
-      "|",
-      "fontSize",
-      "fontFamily",
-      "fontColor",
-      "fontBackgroundColor",
-      "|",
-      "alignment", // <--- ADDED
-      "bulletedList",
-      "numberedList",
-      "|",
-      "blockQuote",
-      "link",
-      "|",
-      "imageUpload",
-      "mediaEmbed",
-      "codeBlock",
-      "|",
-      "undo",
-      "redo",
-    ],
-  },
-  image: {
-    toolbar: [
-      "imageStyle:full",
-      "imageStyle:side",
-      "|",
-      "imageTextAlternative",
-    ],
-  },
-  codeBlock: {
-    languages: [
-      { language: "plaintext", label: "Plain text" }, // The default language.
-      { language: "c", label: "C" },
-      { language: "cs", label: "C#" },
-      { language: "cpp", label: "C++" },
-      { language: "css", label: "CSS" },
-      { language: "diff", label: "Diff" },
-      { language: "html", label: "HTML" },
-      { language: "java", label: "Java" },
-      { language: "javascript", label: "JavaScript" },
-      { language: "php", label: "PHP" },
-      { language: "python", label: "Python" },
-      { language: "ruby", label: "Ruby" },
-      { language: "typescript", label: "TypeScript" },
-      { language: "xml", label: "XML" },
-    ],
-  },
-})
+decouplededitor
+  .create(document.querySelector(".document-editor__editable"), {
+    plugins: [
+      Essentials,
+      UploadAdapter,
+      Autoformat,
+      Bold,
+      Italic,
+      BlockQuote,
+      EasyImage,
+      Heading,
+      Image,
+      ImageCaption,
+      ImageStyle,
+      ImageToolbar,
+      ImageUpload,
+      Link,
+      List,
+      Paragraph,
+      Alignment,
+      Base64UploadAdapter,
+      Font,
+      MediaEmbed,
+      CodeBlock,
+    ], // <--- MODIFIED
+    toolbar: {
+      items: [
+        "heading",
+        "|",
+        "bold",
+        "italic",
+        "|",
+        "fontSize",
+        "fontFamily",
+        "fontColor",
+        "fontBackgroundColor",
+        "|",
+        "alignment", // <--- ADDED
+        "bulletedList",
+        "numberedList",
+        "|",
+        "blockQuote",
+        "link",
+        "|",
+        "imageUpload",
+        "mediaEmbed",
+        "codeBlock",
+        "|",
+        "undo",
+        "redo",
+      ],
+    },
+    image: {
+      toolbar: [
+        "imageStyle:full",
+        "imageStyle:side",
+        "|",
+        "imageTextAlternative",
+      ],
+    },
+    codeBlock: {
+      languages: [
+        { language: "plaintext", label: "Plain text" }, // The default language.
+        { language: "c", label: "C" },
+        { language: "cs", label: "C#" },
+        { language: "cpp", label: "C++" },
+        { language: "css", label: "CSS" },
+        { language: "diff", label: "Diff" },
+        { language: "html", label: "HTML" },
+        { language: "java", label: "Java" },
+        { language: "javascript", label: "JavaScript" },
+        { language: "php", label: "PHP" },
+        { language: "python", label: "Python" },
+        { language: "ruby", label: "Ruby" },
+        { language: "typescript", label: "TypeScript" },
+        { language: "xml", label: "XML" },
+      ],
+    },
+  })
   .then((editor) => {
+    const toolbarContainer = document.querySelector(
+      ".document-editor__toolbar"
+    );
+
+    toolbarContainer.appendChild(editor.ui.view.toolbar.element);
+
     editorDoc = editor;
     $(".editor-date").text(getFormatDate(new Date()));
   })
@@ -148,7 +154,10 @@ $save.on("click", () => {
 $.fn.selectItem = function () {
   $(this).on("click", () => {
     $(".selectNow").removeClass("selectNow");
+    $(".postIcon").removeClass(`${$(this).text().toLowerCase()}`);
+
     $(this).addClass("selectNow");
+    $(".postIcon").addClass(`${$(this).text().toLowerCase()}`);
     $(".selectItem").text($(this).text());
     $postSelectorList.toggleClass("off");
   });
